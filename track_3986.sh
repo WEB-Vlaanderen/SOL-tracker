170675c7fc45f1ff27d24631406ad1042e40fe9e
mslembro <maarten@web.vlaanderen>
2023-03-25 22:57:09 +0100

    22:57:09

--- track_3986.sh
+++ track_3986.sh
@@ -8,4 +8,4 @@ timestamp() {
 curl "https://mijn.web.vlaanderen/custom/shorttrackonline.info/fetch.php?page=compresult&comp=3986&cat=2" > comp3986_cat2.html
 curl "https://mijn.web.vlaanderen/custom/shorttrackonline.info/fetch.php?page=compresult&comp=3986&cat=1" > comp3986_cat1.html
 git commit -a -m "$(timestamp)"
-git push
+git push origin main
