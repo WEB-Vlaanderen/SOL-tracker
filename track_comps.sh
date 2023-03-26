d02aa83dd237060e337d5ee70f9d2e56379f83d6
mslembro <maarten@web.vlaanderen>
2023-03-26 08:54:30 +0200

    2023-03-26_08:54:30

--- track_comps.sh
+++ track_comps.sh
@@ -5,6 +5,8 @@ timestamp() {
   date +%F_%T # current time
 }
 
+cd /home/webvl/domains/webvl.be/public_html/SOL
+
 curl "https://mijn.web.vlaanderen/custom/shorttrackonline.info/fetch.php?page=compresult&comp=3986&cat=2" > comp3986_cat2.html
 curl "https://mijn.web.vlaanderen/custom/shorttrackonline.info/fetch.php?page=compresult&comp=3986&cat=1" > comp3986_cat1.html
 curl "https://mijn.web.vlaanderen/custom/shorttrackonline.info/fetch.php?page=compresult&comp=4006&cat=7" > comp4006_cat7.html
